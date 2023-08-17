65430fb721c514f9af8aad48366fec55bad242ec
tonni-akter-dev <tanniakhter123@gmail.com>
2023-08-17 20:27:33 +0600

    tic-tac-toe game

---
+++ my-react-app/src/components/TicTacToe/Square.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    </>
+  );
+};
+
+export default Square;
