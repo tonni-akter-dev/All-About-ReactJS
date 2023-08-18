43d20e2aad5d031b81330839f5c513f29082ea24
tonni-akter-dev <tanniakhter123@gmail.com>
2023-08-18 20:43:15 +0600

    searchable product

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import { FilterableProductTable } from "./components/SearchableProduct/FilterableProductTable";
 import Game from "./components/TicTacToe/Game";
 import Winner from "./components/TicTacToe/Winner";
-
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
 function App() {
   return (
     <>
-      <p className="read-the-docs">lets Build a tic-tac-toe game</p>
+      {/* <p className="read-the-docs">lets Build a tic-tac-toe game</p>
       <Game />
-      <Winner />
+      <Winner /> */}
+      <FilterableProductTable products={PRODUCTS} />;
     </>
   );
 }
---
+++ my-react-app/src/components/SearchableProduct/FilterableProductTable.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { SearchBar } from "./SearchBar";
+import { ProductTable } from "./ProductTable";
+
+export function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
